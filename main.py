02a9cc3c63e725868debaa15c45ce4fe87c7d3de
Vlad <vladkim1678@gmail.com>
2022-12-28 22:45:32 +0200

    v1

---
+++ main.py
@@ -0,0 +1,18 @@
+class Father:
+    age = 40
+    height = 180
+    satiety = 100
+
+class Mother:
+    age = 37
+    height = 169
+    satiety = 100
+
+class Child:
+    age = 13
+    height = 168
+    satiety = 100
+    def __init__(self):
+        print(self.height)
+        print(self.satiety)
+        print(self.age)
